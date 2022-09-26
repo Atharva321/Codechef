a6882c017619ecee1a413b8309db8bff6e83f322
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-09-26 23:23:50 +0530

    Update Solution.java

--- Equal Distribution/Solution.java
+++ Equal Distribution/Solution.java
@@ -1,1 +1,27 @@
+/* package codechef; // don't place package name! */
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scn = new Scanner(System.in);
+		int T = scn.nextInt();
+		while(T>0){
+		    int Alice = scn.nextInt();
+		    int Bob = scn.nextInt();
+		    if((Alice+Bob)%2 == 0){
+		        System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		    T--;
+		}
+	}
+}
