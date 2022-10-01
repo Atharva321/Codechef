186e9f418b419ff8e842128fa65f040ef8553c10
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-10-01 21:37:56 +0530

    Update Solution.java

--- Chef and Bird farm/Solution.java
+++ Chef and Bird farm/Solution.java
@@ -1,1 +1,78 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		InputReader ir = new InputReader(System.in);
+        	int t = ir.nextInt();
+        	while(t-->0){
+        	    int _Chicken_Legs = ir.nextInt();
+        	    int _Duck_Legs = ir.nextInt();
+        	    int _Total_Legs = ir.nextInt();
+        	    if(_Total_Legs%_Chicken_Legs==0 && _Total_Legs%_Duck_Legs==0){
+        	        System.out.println("ANY");
+        	    }
+        	    else if(_Total_Legs%_Chicken_Legs==0){
+        	        System.out.println("CHICKEN");
+        	    }
+        	    else if(_Total_Legs%_Duck_Legs==0){
+        	        System.out.println("DUCK");
+        	    }
+        	    else {
+        	        System.out.println("NONE");
+        	    }
+        	}
+        	ir.close();
+	}
+}
+
+class InputReader{
+    private BufferedReader br;
+    InputReader(InputStream is){
+        br = new BufferedReader(new InputStreamReader(is));
+    }
+    private StringTokenizer st;
+    public String nextLine(){
+        String s=null;
+        try {
+            s = br.readLine();
+        } catch (Exception e) {
+           
+        }
+        return s;
+    }
+    public String next(){
+        if(st == null || !st.hasMoreTokens()){
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return st.nextToken(" ");
+    }
+
+    public int nextInt(){
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong(){
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble(){
+        return Double.parseDouble(next());
+    }
+    public void close(){
+       try {
+        br.close();
+       } catch (IOException e) {
+        System.out.println(e.getMessage());
+       }
+    }
+}
