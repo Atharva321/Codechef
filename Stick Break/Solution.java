65992f45565a52d790fc4afac158d017afe6c8f3
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-10-01 22:07:57 +0530

    Create Solution.java

---
+++ Stick Break/Solution.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		InputReader ir = new InputReader(System.in);
+        	int t = ir.nextInt();
+        	while(t-->0){
+        	    double _L_Length     = ir.nextDouble();
+        	    double _K_Sticks     = ir.nextDouble();
+        	    if(_L_Length%_K_Sticks==0){
+        	        System.out.println("0");
+        	    }
+        	    else{
+        	        System.out.println("1");
+        	    }
+        	}
+        	ir.close();
+	}
+}
+
+class InputReader{
+    private BufferedReader br;
+    InputReader(InputStream is){
+        br = new BufferedReader(new InputStreamReader(is));
+    }
+    private StringTokenizer st;
+    public String nextLine(){
+        String s=null;
+        try {
+            s = br.readLine();
+        } catch (Exception e) {
+           
+        }
+        return s;
+    }
+    public String next(){
+        if(st == null || !st.hasMoreTokens()){
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return st.nextToken(" ");
+    }
+
+    public int nextInt(){
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong(){
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble(){
+        return Double.parseDouble(next());
+    }
+    public void close(){
+       try {
+        br.close();
+       } catch (IOException e) {
+        System.out.println(e.getMessage());
+       }
+    }
+}
