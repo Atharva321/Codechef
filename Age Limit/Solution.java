13cb828323951d754ee5086fad9772ede968b40a
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-01-19 23:44:31 +0530

    Update Solution.java

--- Age Limit/Solution.java
+++ Age Limit/Solution.java
@@ -3,6 +3,9 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
+// Time  : O(1)
+// Space : O(1)
+
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
